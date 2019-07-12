99333abb3fb569496f195db3f1dbeb6d3c249e9b
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-12 18:00:54 +0100

    🔨 Updates install.

--- install.js
+++ install.js
@@ -1,1 +1,16 @@
 console.log('installing')
+
+var cnf = require('./engine/config')
+var fio = require('./engine/fileIO')
+
+global.logIt = msg => console.info(msg)
+const config = cnf.config()
+
+fio.writesFile(`${config.projectPath}config.js`,
+`exports.values = () => {
+  return {
+    htmlRoot: 'portal',
+    assetsRoot: 'assets',
+    assetFolderExcludes: ['less']
+  }
+}`)
