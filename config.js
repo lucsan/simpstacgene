0aa315260402faee163e56f8a1976b853f1735ef
lucsan <1104946+lucsan@users.noreply.github.com>
2019-07-13 15:34:18 +0100

    🔨 Removes siteBulder override config.js.

--- config.js
+++
@@ -1,9 +0,0 @@
-// To use this override config put it in your project root folder.
-exports.values = () => {
-  return {
-    htmlRoot: 'portal',
-    materialsRoot: 'demo',
-    assetsRoot: 'assets',
-    assetFolderExcludes: ['less']
-  }
-}
